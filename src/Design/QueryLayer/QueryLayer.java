c1fb5d1418775f62dd1a8506d4444df42e3905c6
blunn2 <braden.lunn@gmail.com>
2015-02-22 23:38:00 -0500

    outputs table results as JSONArray.

--- src/Design/QueryLayer/QueryLayer.java
+++ src/Design/QueryLayer/QueryLayer.java
@@ -5,13 +5,15 @@ import java.io.IOException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HTableDescriptor;
-import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.google.protobuf.ServiceException;
 
@@ -21,21 +23,23 @@ public class QueryLayer {
 	public static String HBASE_CONFIGURATION_ZOOKEEPER_CLIENTPORT = "hbase.zookeeper.property.clientPort";
 	public static String HBASE_CONFIGURATION_HBASE_MASTER = "hbase.master";
 
-	public static void main(String[] args) throws IOException, ServiceException {
+	public static void main(String[] args) throws IOException,
+			ServiceException, JSONException {
 		// TODO Auto-generated method stub
 
 		String hbaseZookeeperQuorumIP = "192.168.1.9";
 		String hbaseZookeeperClientPort = "2181";
-		String hbaseMasterIPandPort = "192.168.1.2:60000";
+		String hbaseMasterIPandPort = "192.168.1.9:60000";
 		String tableName = "SensorValues";
 
-		connectToHbase(hbaseZookeeperQuorumIP, hbaseZookeeperClientPort,hbaseMasterIPandPort,
-				tableName);
+		connectToHbase(hbaseZookeeperQuorumIP, hbaseZookeeperClientPort,
+				hbaseMasterIPandPort, tableName);
 	}
 
-	private static void connectToHbase(String hbaseZookeeperQuorumIP,
-			String hbaseZookeeperClientPort, String hbaseMasterIPandPort, String tableName)
-			throws IOException, ServiceException {
+	private static JSONArray connectToHbase(String hbaseZookeeperQuorumIP,
+			String hbaseZookeeperClientPort, String hbaseMasterIPandPort,
+			String tableName) throws IOException, ServiceException,
+			JSONException {
 
 		Configuration hConf = HBaseConfiguration.create();
 		hConf.set(HBASE_CONFIGURATION_ZOOKEEPER_QUORUM, hbaseZookeeperQuorumIP);
@@ -60,18 +64,31 @@ public class QueryLayer {
 
 		ResultScanner scanner = hTable.getScanner(scan);
 
-		for (Result result = scanner.next(); (result != null); result = scanner
-				.next()) 
-		{
-			//gets rowkey
-			System.out.print(Bytes.toString(result.getRow()) + "||");
-			
-			//gets value
-			System.out.println(Bytes.toString(result.getValue(Bytes.toBytes("d"), Bytes.toBytes("val"))));
-		}
+		JSONArray array = new JSONArray();
 
+		for (Result result = scanner.next(); (result != null); result = scanner
+				.next()) {
+			// gets rowkey
+			String rowKey = Bytes.toString(result.getRow());
+			//String sensorID = rowKey.substring(0, rowKey.indexOf("-"));
+			
+			//split row key into id and timestamp
+			String [] rowKeyArray = rowKey.split("-",2);
+
+			String value = Bytes.toString(result.getValue(
+					Bytes.toBytes("d"), Bytes.toBytes("val")));
+			
+			JSONObject obj = new JSONObject();
+			obj.put("id", rowKeyArray[0]);
+			obj.put("timestamp", rowKeyArray[1]);
+			obj.put("value", value);
+			array.put(obj);
+		}
+		
 		scanner.close();
 		hTable.close();
 		hbadmin.close();
+
+		return array;
 	}
 }
